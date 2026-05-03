75413adabcd36bd94814f54d2ce0f37219d595ed
Chris Small <cjs@cjspc.cjs.net>
2026-05-03 19:50:22 +1000

    "stripped leading path."

--- js/init.js
+++ js/init.js
@@ -1,9 +1,9 @@
 // File: /js/init.js
 
-import { consoleLogDisabled, consoleLog, preloadAssets }     from '/js/utils.js';
-import { buildDOM }     from '/js/build.js';
+import { consoleLogDisabled, consoleLog, preloadAssets }     from 'js/utils.js';
+import { buildDOM }     from 'js/build.js';
 export async function initApp() {
-    consoleLogDisabled("Mia: Logging Disabled in ./js/utils.js, only errors and warns will be displayed!");
+    consoleLogDisabled("Mia: Logging Disabled in js/utils.js, only errors and warns will be displayed!");
     consoleLog("[Initialize] [Build DOM]");
     await buildDOM();
 }
