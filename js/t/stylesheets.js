ed0a1afc9f99872a0c81e3f0d44acc3861e4a83d
cjs test <cjt@turnip.cjs.net>
2026-05-04 10:18:06 +1000

    ""

--- js/t/stylesheets.js
+++
@@ -1,9 +0,0 @@
-// File: /js/stylesheets.js
-
-export function loadCSS(href) {
-    const link = document.createElement("link");
-    link.rel = "stylesheet";
-    link.href = href;
-    document.head.appendChild(link);
-}
-
